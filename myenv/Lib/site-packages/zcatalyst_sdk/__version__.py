5d78993dd257aec03b60e20599d37bc2e61d430c
sudhir-sharma-17 <sudhirsh3611@gmail.com>
2026-07-18 01:27:26 +0530

    Untrack myenv folder and update gitignore

--- myenv/Lib/site-packages/zcatalyst_sdk/__version__.py
+++
@@ -1,3 +0,0 @@
-"""Version of zcatalyst SDK"""
-
-__version__ = '1.4.0'
--- myenv/Lib/site-packages/zcatalyst_sdk/job_scheduling/_exception.py
+++
@@ -1,7 +0,0 @@
-from ..exceptions import (
-    CatalystError
-)
-
-class CatalystJobSchedulingError(CatalystError):
-    def __init__(self, code, message, value=None):
-        CatalystError.__init__(self, code, message, value)
